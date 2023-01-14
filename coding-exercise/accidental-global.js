419bf4c73371ea381abfb82db3fbfc22837a00de
Sai Swaroop <saiswaroop874@gmail.com>
2023-01-14 13:08:37 +0300

    practice jS

--- coding-exercise/accidental-global.js
+++ coding-exercise/accidental-global.js
@@ -6,3 +6,15 @@ function foo() {
 }
 
 console.log(foo(), typeof x, typeof y);
+
+// typeof a should return 'undefined' so the first console.log is 'undefined'.
+
+//     However, the second console.log logs 'number' since b is a global variable.
+
+//         Don’t let the let keyword fool us, b is still a global variable since it has no keyword before it.
+
+//             b = 0 is the same as window.b = 0 .
+
+// To avoid this tricky situation, use strict mode by adding 'use strict' to the top of out code.Then we’ll get the error ‘b is not defined’.
+
+// Modules always use strict mode so this is not an issue with them
