b8920cff977745f28906c90823102a192d8e954a
Sai Swaroop <saiswaroop874@gmail.com>
2023-01-18 16:52:43 +0300

    call

---
+++ coding-exercise/call.js
@@ -0,0 +1,10 @@
+let name = {
+      firstname: "sai",
+      lastname: "swaroop"
+      printFullName: function(){
+            console.log(this.firstname + " " + this.lastname);
+      }
+}
+
+name.printFullName();
+
