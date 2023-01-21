e09c7ef63c0598c99c9b6892e0b329bff6dd1843
Sai Swaroop <saiswaroop874@gmail.com>
2023-01-21 11:39:04 +0300

    init

---
+++ coding-exercise/map-filter-reduce.js
@@ -0,0 +1,26 @@
+const users = [
+      {firstname: "sai", lastname: "swaroop", age: 28},
+      {firstname: "elon", lastname: "musk", age: 39},
+      {firstname: "ajay", lastname: "dev", age: 43},
+      {firstname: "satya", lastname: "nadela", age: 48},
+      {firstname: "keerthi", lastname: "sanan", age: 29},
+      {firstname: "depika", lastname: "padukon", age: 29},
+];
+
+const output = users.map( x => x.firstname + " " + x.lastname);
+console.log(output);
+
+// we use reduce when we need one value out of an array
+
+
+// acc = { 28 : 1, 39 : 1, 43 : 1, 48 : 1, 29 : 2 }
+const output2 = users.reduce(function (acc, curr) {
+      if(acc[curr.age]){
+            acc[curr.age] = ++acc[curr.age]
+      }else{
+            acc[curr.age] = 1;
+      }
+      return acc;
+}, {});
+
+console.log(output2);
